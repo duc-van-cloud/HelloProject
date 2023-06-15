5b360859fa5612ddff3764632b70fbc4ae5de708
Duc Van <duc.van@cloud.com>
2023-06-15 14:00:59 +0700

    Init

---
+++ hw.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int feature_2()
+{
+	return 1;
+}
+
+int main(int argc, const char* argv[])
+{
+	printf("Hello world\r\n");
+	return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+int feature_1()
+{
+	/* Feature */
+	return 0;
+}
+
+int main(int argc, const char* argv[])
+{
+	printf("Hello world\r\n");
+	return 0;
+}
